1b00f1e8ff185117aca9d4af1af58bdf7bfefbbb
Sangram Rath <sangram@cloudwrks.in>
2023-01-03 00:19:21 +0530

    update to fix build error in netlify

--- src/app/services/config.ts
+++ src/app/services/config.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { environment } from './src/environments/environment';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class Config {
